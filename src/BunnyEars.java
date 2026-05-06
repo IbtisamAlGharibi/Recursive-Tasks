7333e235bf99c935d095b5422b3c885a3234e819
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 15:28:44 +0400

    check if number is even

--- src/BunnyEars.java
+++ src/BunnyEars.java
@@ -2,6 +2,9 @@ public class BunnyEars {
     public static int bunnyEars2(int n){
         if (n==0){
             return 0;
+        } else if (n % 2 == 0) {
+            return 3;
+
         }
 
     }
