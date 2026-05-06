213f2f38f5cccd9fe8465691c83073e38b3d01f6
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 16:22:48 +0400

    check if its start with pi

--- src/ChangePi.java
+++ src/ChangePi.java
@@ -2,6 +2,8 @@ public class ChangePi {
     public static String  changePi(String str){
         if (str.length() == 0){
             return "";
+        } else if (str.startsWith("pi")) {
+            return "3.14" + changePi(str.substring(2));
         }
     }
 }
