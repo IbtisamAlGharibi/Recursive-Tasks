d110c89ec874f93a916deb5ca0113eccf64e0f8d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 15:55:24 +0400

    CountX method is called and tested

--- src/CountXDemo.java
+++ src/CountXDemo.java
@@ -1,6 +1,8 @@
 public class CountXDemo {
     static CountX countX = new CountX();
     public static void main(String[] args){
-
+        System.out.println(CountX.countX( "xxhixx"));
+        System.out.println(CountX.countX( "xhixhix"));
+        System.out.println(CountX.countX( "hi"));
     }
 }
