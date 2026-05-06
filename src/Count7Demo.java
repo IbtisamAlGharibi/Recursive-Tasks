51fe05183c455fd7711c78fadca5931611325f32
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 15:46:44 +0400

    Count7 method is called and tested

--- src/Count7Demo.java
+++ src/Count7Demo.java
@@ -1,6 +1,8 @@
 public class Count7Demo {
     static Count7 count7 = new Count7();
     public static void main(String[] args){
-
+        System.out.println(Count7.count7(717));
+        System.out.println(Count7.count7(7));
+        System.out.println(Count7.count7(123));
     }
 }
