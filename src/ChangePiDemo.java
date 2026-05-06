a0694b4e4992924ad5f3be13efe8724d4882abec
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 16:26:57 +0400

    ChangePi method is called and tested

--- src/ChangePiDemo.java
+++ src/ChangePiDemo.java
@@ -1,6 +1,8 @@
 public class ChangePiDemo {
     static ChangePi changePi =new ChangePi();
     public static void main(String[] args){
-
+        System.out.println(ChangePi.changePi("xpix"));
+        System.out.println(ChangePi.changePi("pipi"));
+        System.out.println(ChangePi.changePi("pip"));
     }
 }
