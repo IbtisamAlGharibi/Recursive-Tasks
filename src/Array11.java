ae716fa6c0976e0bc9f5d058b18855998bf25fe5
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 16:48:08 +0400

    check if its equal 11

--- src/Array11.java
+++ src/Array11.java
@@ -2,6 +2,8 @@ public class Array11 {
     public static int  array11(int[] nums, int index){
         if ( index == nums.length){
             return 0;
+        } else if (nums[index] == 11) {
+            return 1 + array11(nums, index + 1);
         }
 
     }
