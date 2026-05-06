8eb958ebf26eb47deaa99d97d1b50dbc6bd9a640
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 15:51:52 +0400

    check the last digit

--- src/CountX.java
+++ src/CountX.java
@@ -2,6 +2,8 @@ public class CountX {
     public static int countX(String str){
         if (str.length() == 0){
             return 0;
+        } else if (str.charAt(0)== 'x') {
+            return 1 + countX(str.substring(1));
         }
     }
 }
