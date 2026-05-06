4fa8acea47bacc679048b752f49e4c3d6f644c7f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 16:54:02 +0400

    3 int arrays are created

--- src/Array11Demo.java
+++ src/Array11Demo.java
@@ -1,6 +1,10 @@
 public class Array11Demo {
     static Array11 array11 = new Array11();
     public static void main(String[] args){
+        int[] arr1 = {1, 2, 11};
+        int[] arr2 = {11, 11};
+        int[] arr3 = {1, 2, 3, 4};
 
+        System.out.println(Array11.array11(arr1,2));
     }
 }
