49fc512ef164d4caa03a699b5914d9c35991d4ee
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 15:41:09 +0400

    check if the last digit is 7

--- src/Count7.java
+++ src/Count7.java
@@ -2,6 +2,8 @@ public class Count7 {
     public static int count7(int n){
         if (n==0){
             return 0;
+        } else if (n % 10 == 7 ) {
+            return 1 + count7(n / 10);
         }
     }
 }
