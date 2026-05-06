9a41f4a075253192e91484f5013c519555679d89
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-06 15:34:38 +0400

    BunnyEars methos is called and tested

--- src/BunnyEarsDemo.java
+++ src/BunnyEarsDemo.java
@@ -1,6 +1,10 @@
 public class BunnyEarsDemo {
-static BunnyEars bunnyEars = new BunnyEars();
+   static BunnyEars bunnyEars = new BunnyEars();
 public static void main(String[] args){
+
+    System.out.println(BunnyEars.bunnyEars2(1));
+    System.out.println(BunnyEars.bunnyEars2(0));
+    System.out.println(BunnyEars.bunnyEars2(2));
 
 }
 
