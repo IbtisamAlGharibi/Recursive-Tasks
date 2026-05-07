97b7abf113c8766bcf03457d581e7553145a3525
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 08:46:32 +0400

    CountAbc method is called and tested

--- src/CountAbcDemo.java
+++ src/CountAbcDemo.java
@@ -1,6 +1,8 @@
 public class CountAbcDemo {
     static CountAbc countAbc = new CountAbc();
     public static void main(String[] args){
-
+        System.out.println(CountAbc.countAbc( "abc"));
+        System.out.println(CountAbc.countAbc( "abcxxabc"));
+        System.out.println(CountAbc.countAbc( "abaxxaba"));
     }
 }
