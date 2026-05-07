915d3497d0ea58a32e8099d1dc2478ea25140ede
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 10:30:04 +0400

    check if the first two char is 11

--- src/Count11.java
+++ src/Count11.java
@@ -2,6 +2,8 @@ public class Count11 {
     public static int count11(String str){
         if (str.length()<2){
             return 0;
+        } else if (str.substring(0, 2) == "11") {
+            return 1;
         }
     }
 }
