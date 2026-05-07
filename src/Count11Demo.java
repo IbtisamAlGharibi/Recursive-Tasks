e0ac3af8a3cfee5f9735f63ae791e5ed9defc88f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 10:58:28 +0400

    Count11 method is called and tested

--- src/Count11Demo.java
+++ src/Count11Demo.java
@@ -1,6 +1,8 @@
 public class Count11Demo {
     static Count11 count11 = new Count11();
     public static void main(String[] args){
-
+        System.out.println(Count11.count11("11abc11"));
+        System.out.println(Count11.count11( "abc11x11x11"));
+        System.out.println(Count11.count11("111"));
     }
 }
