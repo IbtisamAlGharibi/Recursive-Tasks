a7bcda64befd24cebb15d55b0504730895dd66fe
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 11:03:53 +0400

    handle if the first char is not (

--- src/ParenBit.java
+++ src/ParenBit.java
@@ -2,6 +2,8 @@ public class ParenBit {
     public static String parenBit(String str){
         if (str.startsWith("(") && str.endsWith(")")){
             return str;
+        } else if (!(str.startsWith("("))) {
+            return str.replace(str.substring(1),"");
         }
     }
 }
