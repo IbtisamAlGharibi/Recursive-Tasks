61684292ce7dc7a88c99bd2e815ed72a9bbb779b
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 08:54:57 +0400

    main method is created

--- src/StrCount.java
+++ src/StrCount.java
@@ -4,7 +4,8 @@ public class StrCount {
         if (str.length() < sub.length()){
             return 0;
         } else if (str.startsWith(sub)) {
-            return  1 + strCount(str.substring(sub.length()), sub);
+            return 1 + strCount(str.substring(sub.length()), sub);
         }
+        return strCount(str.substring(1), sub);
     }
 }
