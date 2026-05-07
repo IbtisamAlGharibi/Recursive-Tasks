8bfe1ab2b707c8b2cb443c601a473c428267d643
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 10:11:36 +0400

    NoX method is called and tested

--- src/NoXDemo.java
+++ src/NoXDemo.java
@@ -1,6 +1,8 @@
 public class NoXDemo {
     static NoX noX = new NoX();
     public static void main(String[] args){
-
+        System.out.println(NoX.noX("xaxb"));
+        System.out.println(NoX.noX( "abc"));
+        System.out.println(NoX.noX("xx"));
     }
 }
