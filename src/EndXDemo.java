703f0b68e42d33236ec371bbe18c08c522ed43d0
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 10:24:32 +0400

    EndX method is called and tested

--- src/EndXDemo.java
+++ src/EndXDemo.java
@@ -1,6 +1,8 @@
 public class EndXDemo {
     static EndX endX = new EndX();
     public static void main(String[] args){
-
+        System.out.println(EndX.endX("xxre"));
+        System.out.println(EndX.endX( "xxhixx"));
+        System.out.println(EndX.endX("xhixhix"));
     }
 }
