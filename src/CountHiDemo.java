36f885535084fc2958cd0c8c894236b042483d34
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 09:58:38 +0400

    CountHi method is called and tested

--- src/CountHiDemo.java
+++ src/CountHiDemo.java
@@ -1,6 +1,8 @@
 public class CountHiDemo {
     static CountHi countHi = new CountHi();
     public static void main(String[] args){
-
+        System.out.println(CountHi.countHi("xxhixx"));
+        System.out.println(CountHi.countHi( "xhixhix"));
+        System.out.println(CountHi.countHi("hi"));
     }
 }
