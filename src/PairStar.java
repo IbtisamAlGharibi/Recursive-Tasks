795870b77087424bf1f296ac7845bfc30e604246
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 08:32:40 +0400

    Task 7: Create a Recursive PairStar Program in Java

--- src/PairStar.java
+++ src/PairStar.java
@@ -5,5 +5,6 @@ public class PairStar {
         } else if (str.charAt(0) == str.charAt(1) ) {
             return str.charAt(0)+ "*" + str.charAt(1);
         }
+        return str.charAt(0) +  str.substring(1,str.length());
     }
 }
