6ffacdb3ff887749ea1a5138aa7fe25422df577d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 08:32:11 +0400

    PairStar method is called and tested

--- src/PairStarDemo.java
+++ src/PairStarDemo.java
@@ -1,6 +1,8 @@
 public class PairStarDemo {
     static PairStar pairStar = new PairStar();
     public static void main(String[] args){
-
+        System.out.println(PairStar.pairStar("hello"));
+        System.out.println(PairStar.pairStar("xxyy"));
+        System.out.println(PairStar.pairStar("aaaa"));
     }
 }
