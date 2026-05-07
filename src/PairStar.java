01b83ffed8ac316b2880bd0be84074bc4064e377
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 08:27:19 +0400

    check if the first char equals the second one

--- src/PairStar.java
+++ src/PairStar.java
@@ -2,6 +2,8 @@ public class PairStar {
     public static String pairStar(String str){
         if (str.length() == 0 || str.length()==1){
             return str;
+        } else if (str.charAt(0) == str.charAt(1) ) {
+            return str.charAt(0)+ "*" + str.charAt(1);
         }
     }
 }
