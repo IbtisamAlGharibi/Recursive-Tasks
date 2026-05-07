3836f92e68ee003684d3b4469b6231c168a9b624
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 11:10:38 +0400

    ParenBit method is called and tested

--- src/ParenBitDemo.java
+++ src/ParenBitDemo.java
@@ -1,6 +1,8 @@
 public class ParenBitDemo {
     static ParenBit parenBit = new ParenBit();
     public static void main(String[] args){
-
+        System.out.println(ParenBit.parenBit("xyz(abc)123"));
+        System.out.println(ParenBit.parenBit( "x(hello)"));
+        System.out.println(ParenBit.parenBit("(xy)1"));
     }
 }
