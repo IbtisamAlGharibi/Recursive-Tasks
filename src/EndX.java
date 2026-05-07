156135613af55943c1c16d59abd0d339aba46548
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 10:18:59 +0400

    handle If the first char is x

--- src/EndX.java
+++ src/EndX.java
@@ -2,6 +2,8 @@ public class EndX {
     public static String  endX(String str){
         if (str.length()==0){
             return " ";
+        }else if (str.charAt(0) == 'x'){
+            return String.valueOf(str.charAt(str.length()) == 'x');
         }
     }
 }
