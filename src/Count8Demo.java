7134acd7e719f7fac838e13ffe489d5a45365e76
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 09:46:13 +0400

    Count8 method is called and tested

--- src/Count8Demo.java
+++ src/Count8Demo.java
@@ -1,6 +1,8 @@
 public class Count8Demo {
     static Count8 count8 = new Count8();
     public static void main(String[] args){
-
+        System.out.println(Count8.count8(8));
+        System.out.println(Count8.count8(818));
+        System.out.println(Count8.count8(8818));
     }
 }
