bb698eb54d48dab187b1b1575b7488361739adae
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 09:53:17 +0400

    Check if the first two characters are Hi

--- src/CountHi.java
+++ src/CountHi.java
@@ -2,6 +2,9 @@ public class CountHi {
     public static int countHi(String str){
         if (str.length()<2){
             return 0;
+        }else if (str.substring(0, 2) =="Hi"){
+            return 1;
         }
+
     }
 }
