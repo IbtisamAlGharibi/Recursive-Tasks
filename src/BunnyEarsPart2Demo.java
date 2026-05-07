93231dffd8096d88c95a4b1192654f57e82548b2
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 09:08:54 +0400

    bunnyEarsPart2 method is called and tested

--- src/BunnyEarsPart2Demo.java
+++ src/BunnyEarsPart2Demo.java
@@ -1,6 +1,8 @@
 public class BunnyEarsPart2Demo {
     static BunnyEarsPart2 bunnyEarsPart2 = new BunnyEarsPart2();
     public static void main(String[] args){
-
+        System.out.println(BunnyEarsPart2.bunnyEars(0));
+        System.out.println(BunnyEarsPart2.bunnyEars(1));
+        System.out.println(BunnyEarsPart2.bunnyEars(2));
     }
 }
