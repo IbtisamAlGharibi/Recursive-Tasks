8293d365104f7d34eb81175f1c2fe8f11cf1e3ba
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 11:15:44 +0400

    compare both strings length

--- src/StrCopies.java
+++ src/StrCopies.java
@@ -2,6 +2,8 @@ public class StrCopies {
     public static boolean strCopies(String str, String sub, int n){
         if (n == 0){
             return true;
+        } else if (str.length()< sub.length()) {
+            return false;
         }
     }
 }
