860e70c15b478f30fa1912da0c2e024f0b64f2ea
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 08:47:06 +0400

    Task 8: Create a Recursive CountAbc Program in Java

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -1,9 +1,12 @@
 public class CountAbc {
     public static int countAbc(String str){
+        int count = 0;
         if (str.length() < 3){
             return 0;
         } else if ((str.charAt(0) == 'a' && str.charAt(1) == 'b' && str.charAt(2) == 'c')
             || (str.charAt(0) == 'a' && str.charAt(1) == 'b' && str.charAt(2) == 'a') ){
+            return count=1;
         }
+      return  countAbc(str.substring(1));
     }
 }
