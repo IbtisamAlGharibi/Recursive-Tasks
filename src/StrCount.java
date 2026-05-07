d30df651ffa3aca3325957213bb892acf203805d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 08:52:17 +0400

    Check if the beginning of str matches sub

--- src/StrCount.java
+++ src/StrCount.java
@@ -1,7 +1,10 @@
 public class StrCount {
     public static int strCount(String str, String sub){
+        int count = 0;
         if (str.length() < sub.length()){
             return 0;
+        } else if (str.startsWith(sub)) {
+            return count = 1;
         }
     }
 }
