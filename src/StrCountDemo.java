42092d8f5844d6c7a552ab89c95fd05ee2fd80ce
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 08:57:34 +0400

    StrCount method is called and tested

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -1,6 +1,8 @@
 public class StrCountDemo {
     static StrCount strCount = new StrCount();
     public static void main(String[] args){
-
+        System.out.println(StrCount.strCount("catcowcat", "cat"));
+        System.out.println(StrCount.strCount("catcowcat", "cow"));
+        System.out.println(StrCount.strCount("catcowcat", "dog"));
     }
 }
