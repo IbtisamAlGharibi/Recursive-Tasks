adbc678d451aa2f67278c2403b16ed8af6a38945
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 11:26:08 +0400

    StrCopies method is called and tested

--- src/StrCopiesDemo.java
+++ src/StrCopiesDemo.java
@@ -1,6 +1,8 @@
 public class StrCopiesDemo {
     static StrCopies strCopies = new StrCopies();
     public static void main(String[] args){
-
+        System.out.println(StrCopies.strCopies("catcowcat","cow",2));
+        System.out.println(StrCopies.strCopies( "catcowcat","cat",3));
+        System.out.println(StrCopies.strCopies("catcowcat","cow",5));
     }
 }
