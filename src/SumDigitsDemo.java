8f3ef5462418cde23019d8358ef8b47eaa7a65f9
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 11:59:01 +0400

    SumDigits method is called and tested

--- src/SumDigitsDemo.java
+++ src/SumDigitsDemo.java
@@ -1,6 +1,8 @@
 public class SumDigitsDemo {
     static SumDigits sumDigits = new SumDigits();
     public static void main(String[] args){
-
+        System.out.println(SumDigits.sumDigits(126));
+        System.out.println(SumDigits.sumDigits(49));
+        System.out.println(SumDigits.sumDigits(12));
     }
 }
