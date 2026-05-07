d0cec2e54be4339a8eff2036623c28c8b71b403f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 10:59:07 +0400

    Task 16: Create a Recursive Count11 Program in Java

--- src/Count11.java
+++ src/Count11.java
@@ -7,5 +7,6 @@ public class Count11 {
         }else if (str.substring(1,3) == "11") {
             return 1 + count11(str.substring(2));
         }
+        return count11(str.substring(1));
     }
 }
