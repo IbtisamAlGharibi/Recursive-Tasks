219030046c33ebc38b5cfd22aab86c552819f056
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 08:39:03 +0400

    check the sequence

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -2,6 +2,8 @@ public class CountAbc {
     public static int countAbc(String str){
         if (str.length() < 3){
             return 0;
+        } else if ((str.charAt(0) == 'a' && str.charAt(1) == 'b' && str.charAt(2) == 'c')
+            || (str.charAt(0) == 'a' && str.charAt(1) == 'b' && str.charAt(2) == 'a') ){
         }
     }
 }
