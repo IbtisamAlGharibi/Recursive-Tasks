b0fa558ad5f19d504218f8ca720be123988a70bd
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 09:39:00 +0400

    2 int variables are declared

--- src/Count8.java
+++ src/Count8.java
@@ -1,5 +1,7 @@
 public class Count8 {
     public static int count8(int n){
+        int lastDigit= n % 10;
+        int secondDigit =  (n / 10) % 10;
         if (n == 0){
             return 0;
         }
