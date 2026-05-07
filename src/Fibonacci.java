b48b4ef78197dab523e63b29586341b15be9bc09
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 11:33:02 +0400

    base case is defined

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,5 +1,9 @@
 public class Fibonacci {
     public static int fibonacci(int n){
-
+        if (n==0 ){
+            return 0;
+        } else if (n==1) {
+            return 1;
+        }
     }
 }
