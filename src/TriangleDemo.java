e30513559a158b589396032992479257686f49bd
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 09:35:41 +0400

    Triangle method is called and tested

--- src/TriangleDemo.java
+++ src/TriangleDemo.java
@@ -1,6 +1,8 @@
 public class TriangleDemo {
     static Triangle triangle = new Triangle();
     public static void main(String[] args){
-
+        System.out.println(Triangle.triangle(0));
+        System.out.println(Triangle.triangle(1));
+        System.out.println(Triangle.triangle(2));
     }
 }
