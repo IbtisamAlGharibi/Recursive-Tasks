5a3e059966d3c58788e5a30afd0d0727a83647e7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-05-07 11:38:44 +0400

    fibonacci method is called and tested

--- src/FibonacciDemo.java
+++ src/FibonacciDemo.java
@@ -2,6 +2,10 @@ public class FibonacciDemo {
     static Fibonacci fibonacci = new Fibonacci();
 
     public static void main(String[] args){
+        System.out.println(Fibonacci.fibonacci(0));
+        System.out.println(Fibonacci.fibonacci( 1));
+        System.out.println(Fibonacci.fibonacci(2));
+        System.out.println(Fibonacci.fibonacci(10));
 
     }
 }
